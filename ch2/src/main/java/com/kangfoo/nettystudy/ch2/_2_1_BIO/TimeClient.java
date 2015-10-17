359a68f7675dedfa0b58d227365ffafdcd558678
Dong lei <d21958@yeah.net>
2015-10-17 17:34:05 +0800

    修改

--- ch2/src/main/java/com/kangfoo/nettystudy/ch2/_2_1_BIO/TimeClient.java
+++ ch2/src/main/java/com/kangfoo/nettystudy/ch2/_2_1_BIO/TimeClient.java
@@ -13,56 +13,58 @@ import java.net.Socket;
  */
 public class TimeClient {
 
-    public static void main(String[] args) {
-        int port = 8080;
-        if (args != null && args.length > 0) {
-            try {
-                port = Integer.valueOf(args[0]);
-            } catch (NumberFormatException e) {
-                // TODO 请自行扩展.
-            }
-        }
+	public static void main(String[] args) {
+		int port = 8080;
+		if (args != null && args.length > 0) {
+			try {
+				port = Integer.valueOf(args[0]);
+			} catch (NumberFormatException e) {
+				// TODO 请自行扩展.
+			}
+		}
 
-        Socket socket = null;
-        BufferedReader in = null;
-        PrintWriter out = null;
+		Socket socket = null;
+		BufferedReader in = null;
+		PrintWriter out = null;
 
-        try {
-            socket = new Socket("localhost", port);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(socket.getOutputStream(), true);
-            out.println("QUERY TIME ORDER");
-            System.out.println("Send order 2 server succeed.");
-            String resp = in.readLine();
-            System.out.println("Now is : " + resp);
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } finally {
-            if (out != null) {
-                out.close();
-                out = null;
-            }
+		try {
+			socket = new Socket("localhost", port);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(), true);
 
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+			while (true) {
+				out.println("QUERY TIME ORDER");
+				System.out.println("Send order 2 server succeed.");
+				String resp = in.readLine();
+				System.out.println("Now is : " + resp);
+			}
+		} catch (IOException e) {
+			e.printStackTrace(); //To change body of catch statement use File | Settings | File Templates.
+		} finally {
+			if (out != null) {
+				out.close();
+				out = null;
+			}
 
-            }
+			if (in != null) {
+				try {
+					in.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 
-            if (socket != null) {
-                try {
-                    socket.close();
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-                socket = null;
-            }
-        }
+			}
 
+			if (socket != null) {
+				try {
+					socket.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				socket = null;
+			}
+		}
 
-    }
+	}
 
 }
